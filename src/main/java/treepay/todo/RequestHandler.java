e6a31254ac5cbf180c257cf55a9810a3158d1b64
Thachapong Chirarattanawong <thachapong.chi@ascendcorp.com>
2017-03-08 01:47:59 +0700

    First commit.

---
+++ src/main/java/treepay/todo/RequestHandler.java
@@ -0,0 +1,115 @@
+package treepay.todo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
+
+import treepay.todo.util.DatabaseUtil;
+
+public class RequestHandler implements RequestStreamHandler {
+
+    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context) throws IOException {
+    	
+    	System.out.println("Loading Java Lambda handler of RequestHandler.");
+    	
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        JSONObject handlerResponse = null;
+
+        try {
+        	JSONTokener token = new JSONTokener(reader);
+        	JSONObject event = new JSONObject(token);
+
+    		System.out.println(" event: " + event.toString());
+
+            String receive_httpMethod = (String) event.get("httpMethod");
+            
+
+        	Connection conn = null;
+        	String connectionString = System.getenv("connection_string");
+            try {
+    			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+    			conn = DriverManager.getConnection(connectionString);
+    		} catch (Exception e1) {
+    			// TODO Auto-generated catch block
+    			e1.printStackTrace();
+    		}
+            String id = null;
+            if(event.get("pathParameters")!=null && !event.getString("pathParameters").equals("null"))
+            	id = event.getJSONObject("pathParameters").getString("id");
+            
+            JSONObject body = null;
+            if(event.get("body")!=null && !event.getString("body").equals("null"))
+            	body = new JSONObject(event.getString("body"));
+            
+            String sqlString = DatabaseUtil.getSqlString(receive_httpMethod, id, body);
+            JSONObject returnResult = DatabaseUtil.exceuteSql(sqlString, conn);
+            
+            try {
+                if("true".equals(returnResult.get("is_updated")))
+                	returnResult.put("data", body);
+			} catch (JSONException e) {
+				// Do nothing.
+			}
+            
+        	handlerResponse = new JSONObject();
+
+        	JSONObject response_hearder = new JSONObject();
+            response_hearder.put("x-custom-response-header", "my custom response header value");
+            
+        	handlerResponse.put("statusCode", "200");
+        	handlerResponse.put("headers", response_hearder);
+        	handlerResponse.put("body", returnResult.toString());
+
+        } catch(Exception e) {
+        	e.printStackTrace();
+			try {
+				handlerResponse = new JSONObject();
+				handlerResponse.put("statusCode", "400");
+	            handlerResponse.put("exception", e);
+			} catch (JSONException exjson) {
+				exjson.printStackTrace();
+			}
+        }
+
+		System.out.println(" handlerResponse: " + handlerResponse.toString());
+    	OutputStreamWriter writer = new OutputStreamWriter(outputStream, "UTF-8");
+    	writer.write(handlerResponse.toString());
+    	writer.close();
+    }
+    
+    
+    
+    public static void main(String[] args) throws SQLException {   
+    	
+    	Connection conn = null;
+    	String connectionString = "jdbc:mysql://dbone.cbvpxlkpqfey.ap-southeast-1.rds.amazonaws.com/test?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&user=root&password=rootroot";
+        try {
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			conn = DriverManager.getConnection(connectionString);
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+    	String sqlInsert = "INSERT INTO todo (name, description, is_done, due_date, order_id) "
+    					 + "VALUES ('My wedding', 'My wedding party at Lampang', '0', '20170319', 1)";
+    	String sqlSelect = "SELECT * from todo where id=1";
+    	JSONObject returnResult = DatabaseUtil.exceuteSql(sqlInsert, conn);
+    	System.out.println(returnResult);
+    	conn.close();
+    }
+    
+}
---
+++ src/main/java/treepay/todo/util/DatabaseUtil.java
@@ -0,0 +1,98 @@
+package treepay.todo.util;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.mysql.cj.jdbc.PreparedStatement;
+
+public class DatabaseUtil {
+	
+	public static String getSqlString(String receive_httpMethod, String id, JSONObject data) throws JSONException{
+        String sqlString = "";
+        if(receive_httpMethod.equals("GET") && id != null){
+        	sqlString = "SELECT * FROM todo WHERE id=" + id;
+        }if(receive_httpMethod.equals("GET") && id == null){
+        	sqlString = "SELECT * FROM todo";
+        }else if(receive_httpMethod.equals("POST")){
+        	sqlString = "INSERT INTO todo (name, description, is_done, due_date, order_id) "
+        					 + "VALUES ('"+data.getString("name")+"', '"+data.getString("description")+"', '"+data.getString("is_done")+"', '"+data.getString("due_date")+"', "+data.getString("order_id")+")";
+        }else if(receive_httpMethod.equals("PUT")){
+        	sqlString = "UPDATE todo SET name='"+data.getString("name")+"', description='"+data.getString("description")+"', is_done='"+data.getString("is_done")+"', due_date='"+data.getString("due_date")+"', order_id='"+data.getString("order_id")+"' "
+        					 + "WHERE id=" + ((id!=null)? id : data.getString("id"));
+        }else if(receive_httpMethod.equals("DELETE")){
+        	sqlString = "DELETE FROM WHERE id=" + ((id!=null)? id : data.getString("id"));
+        }
+        System.out.println("sqlString: "+sqlString);
+		return sqlString;
+	}
+	
+    public static JSONObject exceuteSql(String sqlString, Connection conn) {
+//    	PreparedStatement prest;
+//        prest = conn.prepareStatement("", Statement.RETURN_GENERATED_KEYS);
+//        prest.setString(1,"abc");
+//        prest.setInt(2,123);prest.executeUpdate();
+//        ResultSet rs = prest.getGeneratedKeys();
+//        if(rs.next())
+//        {
+//            int last_inserted_id = rs.getInt(1);
+//        }
+
+    	JSONObject result = new JSONObject();
+    	Statement stmt = null;
+    	ResultSet rs = null;
+    	try {
+    	    
+    	    stmt = conn.createStatement();
+    	    if(sqlString.startsWith("SELECT")){
+    	    	
+    	    	rs = stmt.executeQuery(sqlString);
+    	    	
+        	    while (rs != null && rs.next()) {
+                    result.put("id", rs.getString("id"));
+                    result.put("name", rs.getString("name"));
+                    result.put("description", rs.getString("description"));
+                    result.put("is_done", rs.getString("is_done"));
+                    result.put("due_date", rs.getString("due_date"));
+                    result.put("order_id", rs.getString("order_id"));
+                }
+    	    }
+    	    else{
+    	    	int returnResult = stmt.executeUpdate(sqlString);
+    	    	result.put("is_updated", returnResult==1?"true":"false");
+    	    }
+    	    
+    	    
+    	} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+		    // it is a good idea to release
+		    // resources in a finally{} block
+		    // in reverse-order of their creation
+		    // if they are no-longer needed
+
+		    if (rs != null) {
+		        try { rs.close(); } 
+		        catch (SQLException sqlEx) { } // ignore
+		        rs = null;
+		    }
+		    if (stmt != null) {
+		        try { stmt.close(); } 
+		        catch (SQLException sqlEx) { } // ignore
+		        stmt = null;
+		    }
+		    if (conn != null) {
+		        try { conn.close(); } 
+		        catch (SQLException sqlEx) { } // ignore
+		        conn = null;
+		    }
+		}
+    	
+    	return result;
+    }
+}
